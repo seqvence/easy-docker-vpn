ba8e99a110793be820ecf932b1b4d9c79c25fc6c
Nate Jones <nate@endot.org>
2016-10-09 13:14:46 -0700

    add support for running in a container

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# if attempting to cat out the wrapper, let that through, otherwise proxy to easy_docker_vpn
+if [ "$1" == "cat" ]; then
+	eval "$@"
+else
+	eval "$(/code/bin/easy_docker_vpn init -)"
+
+	eval easy_docker_vpn "$@"
+fi
